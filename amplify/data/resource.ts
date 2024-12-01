f6f83781e72dcd7b8efc35e4e91cc15d19d09998
sz44 <semion44@gmail.com>
2024-12-01 14:59:19 -0800

    isDone fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,7 +10,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-      idSone: a.boolean(),
+      isDone: a.boolean(),
     })
     .authorization((allow) => [allow.owner()]),
 });
